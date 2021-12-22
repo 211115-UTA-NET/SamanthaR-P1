1ef345eabb2ca0bc3d8594c6c86f3ac764d1a2e5
srod-85 <94401097+srod-85@users.noreply.github.com>
2021-12-22 04:53:25 -0800

    4:54am

--- P0_Store/Program.cs
+++ P0_Store/Program.cs
@@ -17,27 +17,68 @@ namespace P0Store
             string connnectionString = File.ReadAllText("C:/Users/roder/Revature/BookDBConnectionString.txt");
             using SqlConnection connection = new(connnectionString); 
             //Fields
-            string firstName;
-            string lastName;
-            string shippingAddress;
-            string shippingCity;
-            string shippingState;
+            
             int userStatueSelection;
             int userDesiredQty;
             string[] stores = {"Seattle", "Portland", "Sacramento"};
             int storeChoice = 0;
+            string response;
+            bool verifyCustomer = false;
 
             WriteLine("Welcome to the Garden Ceramics Store for Misfit Toys!");
-            WriteLine("Please enter your first name.");
-            firstName = ReadLine();
-            WriteLine("Please enter your last name");
-            lastName = ReadLine();
-
-            connection.Open();
-            string commandText = $"INSERT INTO Garden_Customer (Customer_First_Name, Customer_Last_Name, Shipping_Address, Shipping_City, Shipping_State) VALUES ('{firstName}', '{lastName}', '{shippingAddress}', '{shippingCity}', '{shippingState}');";
-            using SqlCommand command1 = new(commandText, connection);
-            connection.Close();
-            WriteLine($"It's nice to meet you, {firstName} {lastName}! \nWhat store do you prefer to make your orders to? \nPlease select from the following:\nFor Seattle, enter 1 \nFor Portland, enter 2 \nFor Sacramento, enter 3");
+        do{
+            WriteLine("Are you a new customer? y/n?");
+            response = ReadLine();
+            string yesno = response.ToLower();
+            if(yesno == "y")
+            {
+                
+                WriteLine("Please enter your first name.");
+                string firstName = ReadLine();
+                WriteLine("Please enter your last name");
+                string lastName = ReadLine();
+                WriteLine("What is the first line of your address for shipping?");
+                string shippingAddress= ReadLine();
+                WriteLine("What is your city?");
+                string shippingCity = ReadLine();
+                WriteLine("What is your state?");
+                string shippingState = ReadLine();
+            
+                connection.Open();
+                string commandText1 = $"INSERT INTO Garden_Customer (Customer_First_Name, Customer_Last_Name, Shipping_Address, Shipping_City, Shipping_State) VALUES ('{firstName}', '{lastName}', '{shippingAddress}', '{shippingCity}', '{shippingState}');";
+                using SqlCommand command1 = new(commandText1, connection);
+                connection.Close();
+                WriteLine($"It's nice to meet you, {firstName} {lastName}!");
+                verifyCustomer = true;
+                break; 
+            } 
+            else 
+            {
+                WriteLine("Let's look you up in our database. What is your first name?");
+                string firstName = ReadLine();
+                WriteLine("What is your last name?");
+                string lastName = ReadLine();
+                WriteLine("What is your city?");
+                string shippingCity = ReadLine();
+                connection.Open();
+                string commandText2 = $"SELECT * FROM Garden_Customer WHERE Customer_First_Name = '{firstName}' AND Customer_Last_Name = '{lastName}' AND Shipping_City = '{shippingCity}';";
+                WriteLine("Printing search results... \n-----------------------------------------");
+                using SqlCommand command5 = new(commandText2, connection);
+                using SqlDataReader reader2 = command5.ExecuteReader();
+                reader2.Read();
+                connection.Close();
+                WriteLine("----------------------------------------- \nDid we find you? y/n");
+                string verifyCorrect = ReadLine();
+                yesno = verifyCorrect.ToLower();
+                if (yesno == "y")
+                        verifyCustomer = true;
+                else
+                        WriteLine("You are a new customer. We will need to add you to our database.");
+                }
+        }while(!verifyCustomer);
+            //while(verifyCustomer == false);
+            
+            WriteLine("\nWhat store are you making your order from today? \nPlease select from the following:\nFor Seattle, enter 1 \nFor Portland, enter 2 \nFor Sacramento, enter 3");
             
             while(storeChoice != null)
             {
@@ -57,50 +98,47 @@ namespace P0Store
                     continue;
                 }
             }
-            WriteLine("What is the first line of your address for shipping?");
-            shippingAddress= ReadLine();
-            WriteLine("What is your city?");
-            shippingCity = ReadLine();
-            WriteLine("What is your state?");
-            shippingState = ReadLine();
+            
 
             // do {
             //     WriteLine("");
             // }while();
             
-            using SqlConnection connection = new(connnectionString); 
-            connection.Open();
-            commandText = "SELECT * FROM Statue;";
+            using SqlConnection connection3 = new(connnectionString); 
+            connection3.Open();
+            string commandText = "SELECT * FROM Statue;";
             
             //then insert those variables into the appropriate information by columns of the customer table
  
-            using SqlCommand command2 = new(commandText, connection);
+            using SqlCommand command2 = new(commandText, connection3);
             //take this command and put it through this connection
 
-            //input validation printline
             using SqlDataReader reader = command2.ExecuteReader();
 
             while (reader.Read()) //reads one row at a time, getting one value for each column, indexes at [0], you must know the type and the value that will come out of the database
             {
     
-                int itemID = reader.GetInt32(0);
+                int item_ID = reader.GetInt32(0);
 
                 string style = reader.GetString(1);
     
                 decimal price = reader.GetDecimal(2);
 
-                Console.WriteLine($"Enter {itemID} to select '{style}' for ${price}");
-            }
+                Console.WriteLine($"Enter {item_ID} to select '{style}' for ${price}");
+             }
+
             
-            //decimal itemPrice
-            //int quantity
+
             //decimal total = 0;
+            userStatueSelection = Convert.ToInt32(ReadLine());
+            int itemID = userStatueSelection;
+            WriteLine($"You selected {itemID}. How many would you like to order?");
             connection.Close();
             //use a do-while loop -- so they can continue to shop if they want more items
             //have a readline for the user's desired option and quantity
             //want to take in a user option and quantity and create a statement that can 1. connection.open(), 2. reduce the inventory at the selected store by their quantity choice, 3. connection.close() 
             //total += qty*price
-        } //(while the user's input != 1,2,3,4,or5)
+        //} (while the user's input != 1,2,3,4,or5)
             //
             /* connection.Open;
             
@@ -119,5 +157,5 @@ namespace P0Store
             
             
             */
-    }
-}
+           
+}}}
