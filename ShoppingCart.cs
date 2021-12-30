27419f97c0a11605d3960f678e3695e70fb4ed87
srod-85 <94401097+srod-85@users.noreply.github.com>
2021-12-30 14:59:03 -0800

    Can write customer info into DB

---
+++ ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Console;
+
+namespace P0Store
+{
+    class ShoppingCart
+    {
+
+        //Method
+
+
+    }
+}
